b3a01c49848bbc03efd2f29721de72358d882cd7
hanson.wang <hanson.wng@gmail.com>
2013-10-18 19:12:13 -0400

    Forgot some files.

---
+++ LoLStats/Logger.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.IO;
+using log4net;
+
+namespace LoLStats
+{
+  class Logger
+  {
+    private static ILog log;
+    static Logger() {
+      var pattern = "%date %level - %message %exception%newline";
+      var appender = new log4net.Appender.RollingFileAppender {
+        Layout = new log4net.Layout.PatternLayout(pattern),
+        File = "log.txt",
+        AppendToFile = true,
+        RollingStyle = log4net.Appender.RollingFileAppender.RollingMode.Composite,
+        MaxSizeRollBackups = 1,
+        MaximumFileSize = "1MB",
+        StaticLogFileName = true
+      };
+      appender.ActivateOptions();
+      log4net.Config.BasicConfigurator.Configure(appender);
+      log = LogManager.GetLogger("LoLStats");
+    }
+
+    public static void LogException(Exception e) {
+      if (log.IsErrorEnabled) {
+        while (e.InnerException != null) e = e.InnerException;
+        log.Error("Exception:", e);
+      }
+    }
+
+    public static void LogMessage(string message, bool error = false) {
+      if (log.IsErrorEnabled) {
+        if (error) {
+          log.Error(message);
+        } else {
+          log.Info(message);
+        }
+      }
+    }
+  }
+}
